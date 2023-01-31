e54cec283fe8514d94243eeedf583a76cc44c2ef
Mohamed ShamsEldeen <mmm.emad64@gmail.com>
2023-01-31 03:52:41 +0200

    lesson 1

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//Important The tools..
+import * as THREE from 'three';
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+);
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+camera.position.set(-10, 30, 30);
+orbit.update();
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({
+    color: 0x00FF00
+});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    side: THREE.DoubleSide,
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = 0.5 * Math.PI;
+
+
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50)
+const sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0000FF,
+    wireframe: false,
+})
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+scene.add(sphere);
+sphere.position.set(-10, 10, 0);
+
+const animate = (time) => {
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
